9c036fccaf0c4e83d46a24d019a3e605d9fa8e4e
Dmitriy <apuc06@mail.ru>
2019-12-18 08:34:17 +0300

    r2d2

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,10 @@
+function accordionInit() {
+    $(document).on('click', '.faq__accordion-title a', function (e) {
+        e.preventDefault();
+        $(this).toggleClass('active');
+        $(this).parent().next().slideToggle();
+    })
+}
 $(function () {
-
+    accordionInit();
 });
